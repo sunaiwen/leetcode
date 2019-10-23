0258da0c87f1722900ebd59d415c8e27f6e07c15
sunaiwen <sunivengg@gmail.com>
2019-10-23 23:17:03 +0800

    two-sum hash table approach

---
+++ 1.two-sum.js
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum (nums, target) {
+  const map = {}
+  const ret = []
+
+  const len = nums.length
+  for (let i = 0; i < len; i++) {
+    if (typeof map[nums[i]] !== 'undefined') {
+      map[nums[i]] = [map[nums[i]]]
+      map[nums[i]].push(i)
+    } else {
+      map[nums[i]] = i
+    }
+  }
+
+  for (let i = 0; i < len; i++) {
+    const left = target - nums[i]
+
+    if (typeof map[left] !== 'undefined' && map[left] !== i) {
+      if (typeof map[left] === 'object') {
+        for (let j = 0; j < map[left].length; j++) {
+          if (map[left][j] !== i) {
+            ret.push(map[left][j])
+            break
+          }
+        }
+      } else {
+        ret.push(map[left])
+      }
+    }
+  }
+
+  return ret
+}
+
