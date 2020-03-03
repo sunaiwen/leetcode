9a4ca56dddf94fd062292b10ee195b9f3af98a3b
sunaiwen <sunivengg@gmail.com>
2020-03-03 08:29:37 +0800

    chore: add solution 2

--- 2.add-two-numbers.js
+++ 2.add-two-numbers.js
@@ -76,6 +76,33 @@ var addTwoNumbers = function(l1, l2) {
   return startPoint
 };
 
+// 思路二：来自 leetcode 这个问题的 discussion。用递归，然后把前一轮的大于 10 的计算传入下一轮。
+// 这样就省去了一次 if，也不用想着如何保存 link list 的 start point。
+// 参考：https://leetcode.com/problems/add-two-numbers/solution/
+
+/**
+
+
+/**
+var addTwoNumbers = function(l1, l2) {
+  let node = null
+  const carry = arguments[2]
+  if (l1 || l2) {
+      const val1 = l1 ? l1.val : 0
+      const val2 = l2 ? l2.val : 0
+      const next1 = l1 ? l1.next : null
+      const next2 = l2 ? l2.next : null
+      const val = carry ? val1 + val2 + 1 : val1 + val2
+      node = new ListNode(val % 10)
+      node.next = addTwoNumbers(next1, next2, val >= 10)  
+  } else if (carry) {
+      node = new ListNode(1)
+      node.next = null
+  }
+  return node
+};
+*/
+
 // test
 // function ListNode (val) {
 //   this.val = val
