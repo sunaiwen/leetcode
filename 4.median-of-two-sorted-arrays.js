7a89192211cab2c5b4960ec0d9b738c77d666091
sunaiwen <sunivengg@gmail.com>
2020-04-05 23:57:14 +0800

    4.median-of-two-sorted-array: try to understand...

---
+++ 4.median-of-two-sorted-arrays.js
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=4 lang=javascript
+ *
+ * [4] Median of Two Sorted Arrays
+ *
+ * https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+ *
+ * algorithms
+ * Hard (26.96%)
+ * Likes:    5239
+ * Dislikes: 762
+ * Total Accepted:    528.4K
+ * Total Submissions: 1.9M
+ * Testcase Example:  '[1,3]\n[2]'
+ *
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ * 
+ * Find the median of the two sorted arrays. The overall run time complexity
+ * should be O(log (m+n)).
+ * 
+ * You may assume nums1 and nums2 cannot be both empty.
+ * 
+ * Example 1:
+ * 
+ * 
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * 
+ * The median is 2.0
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * 
+ * The median is (2 + 3)/2 = 2.5
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+  const aLen = nums1.length
+  const bLen = nums2.length
+  const halfLen = Math.round((aLen + bLen) / 2)
+  let aCount = Math.round(halfLen / 2)
+  let bCount = halfLen - aCount
+
+  
+};
+// @lc code=end
+
