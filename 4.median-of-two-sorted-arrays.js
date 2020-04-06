a09d720228e7ec0c7b6c99ddec82ec4a26ea4c68
sunaiwen <sunivengg@gmail.com>
2020-04-06 13:13:28 +0800

    feat: 4.median-of-two-sorted-arrays

--- 4.median-of-two-sorted-arrays.js
+++ 4.median-of-two-sorted-arrays.js
@@ -47,13 +47,113 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
+  let aLen = nums1.length
+  let bLen = nums2.length
+  const halfLen = Math.ceil((aLen + bLen) / 2)
+  const isOdd = ((aLen + bLen) % 2 > 0)
+
+  if (nums1[aLen - 1] <= nums2[0] || (aLen === 0 && bLen > 0)) {
+    return handleTwoArrayIsConcat(nums1, nums2)
+  }
+
+  if (nums2[bLen - 1] <= nums1[0] || (bLen === 0 && aLen > 0)) {
+    return handleTwoArrayIsConcat(nums2, nums1)
+  }
+
+  if (aLen < bLen) {
+    let temp = nums1
+    nums1 = nums2
+    nums2 = temp
+
+    temp = aLen
+    aLen = bLen
+    bLen = temp
+  }
+
+  let aMinCount = 0
+  let aMaxCount = aLen
+
+  while (aMinCount <= aMaxCount) {
+    let aCount = Math.floor((aMinCount + aMaxCount) / 2)
+    if (aCount > halfLen) {
+      aCount = halfLen
+    }
+    let bCount = halfLen - aCount
+    if (bCount > halfLen) {
+      bCount = halfLen
+      aCount = 0
+    }
+    if (bCount > bLen) {
+      bCount = bLen
+      aCount = halfLen - bCount
+    }
+
+    const numberA = nums1[aCount - 1]
+    const nextNumberA = aCount < aLen ? nums1[aCount] : undefined
+    const numberB = nums2[bCount - 1]
+    const nextNumberB = nums2[bCount]
+
+    if (numberA > nextNumberB) {
+      aMaxCount = aCount - 1
+    } else if (numberB > nextNumberA) {
+      aMinCount = aCount + 1
+    } else {
+      let medianEndNumber
+      if (numberA == null) {
+        medianEndNumber = numberB
+      } else if (numberB == null) {
+        medianEndNumber = numberA
+      } else {
+        medianEndNumber = Math.max(numberA, numberB)
+      }
+
+      if (isOdd) {
+        return medianEndNumber
+      }
+
+      let medianEndNextNumber = nextNumberA == null ? nextNumberB : nextNumberB == null ? nextNumberA : Math.min(nextNumberA, nextNumberB)
+      return (medianEndNumber + medianEndNextNumber) / 2
+    }
+  }
+};
+
+function handleTwoArrayIsConcat (nums1, nums2) {
   const aLen = nums1.length
   const bLen = nums2.length
-  const halfLen = Math.round((aLen + bLen) / 2)
-  let aCount = Math.round(halfLen / 2)
-  let bCount = halfLen - aCount
+  const halfLen = Math.ceil((aLen + bLen) / 2)
+  const isOdd = ((aLen + bLen) % 2 > 0)
 
-  
-};
+  let bCount = halfLen - aLen
+  let aCount = aLen
+  if (bCount < 0) {
+    aCount = halfLen
+    bCount = 0
+    if (isOdd) {
+      return nums1[aCount - 1]
+    } else {
+      return (nums1[aCount - 1] + nums1[aCount]) / 2
+    }
+  } else if (bCount === 0) {
+    if (isOdd) {
+      return nums1[aCount - 1]
+    } else {
+      return (nums1[aCount - 1] + nums2[0]) / 2
+    }
+  } else {
+    if (isOdd) {
+      return nums2[bCount - 1]
+    } else {
+      return (nums2[bCount - 1] + nums2[bCount]) / 2
+    }
+  }
+}
+
+// console.log(findMedianSortedArrays([1,5,7,9,12,32,44,55, 56,89,120], [12,32,43,53,67,89,100,230,231]))
+// console.log(findMedianSortedArrays([1, 2], [-1, 3]))
+// console.log(findMedianSortedArrays([2], [1,3,4]))
+// console.log(findMedianSortedArrays([4], [1,2,3,5]))
+// console.log(findMedianSortedArrays([4], [1,2,3,5,6]))
+// console.log(findMedianSortedArrays([1, 3], [2, 4, 5, 6]))
+// console.log(findMedianSortedArrays([1,5], [2,3,4,6,7]))
 // @lc code=end
 
