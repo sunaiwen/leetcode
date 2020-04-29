18fa97b7c16c79589666add7e97cef41ad0659c1
sunaiwen <sunivengg@gmail.com>
2020-04-29 23:58:53 +0800

    feat: One other practice

---
+++ others/面试题56 - I. 数组中数字出现的次数.js
@@ -0,0 +1,62 @@
+/* 
+一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。
+
+要求时间复杂度是O(n)，空间复杂度是O(1)。
+
+
+示例 1：
+
+输入：nums = [4,1,4,6]
+输出：[1,6] 或 [6,1]
+
+示例 2：
+
+输入：nums = [1,2,10,4,1,4,3,3]
+输出：[2,10] 或 [10,2]
+
+
+限制：
+
+    2 <= nums <= 10000
+
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumbers = function(nums) {
+  let tmp = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    tmp ^= nums[i]
+  }
+
+  let distintion = 1
+
+  while (tmp & distintion === 0) {
+    distintion <<= 1
+  }
+
+  let a = 0
+  let b = 0
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i]
+
+    if (n & distintion === 0) {
+      a ^= n
+    } else {
+      b ^= n
+    }
+  }
+
+  return [a, b]
+};
+
+console.log(singleNumbers([1,2,10,4,1,4,3,3]))
