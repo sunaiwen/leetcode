67c3dcc898d5f290f16251129ace3af9127bb159
sunaiwen <sunivengg@gmail.com>
2020-10-21 23:03:49 +0800

    refactor: 17 with BFS

--- 17.letter-combinations-of-a-phone-number.js
+++ 17.letter-combinations-of-a-phone-number.js
@@ -22,27 +22,55 @@ const map = {
   '9': 'wxyz'
 }
 
-// DFS
+// BFS
 var letterCombinations = function(digits) {
   if(!digits) {
     return []
   }
-  const ret = []
-  combind(0, digits, '', ret)
-  return ret
-};
-
-function combind(i, digits, curCombination, ret) {
-  if(i == digits.length) {
-    ret.push(curCombination)
-    return ret
+  let idx = 0
+  let prev = []
+  while (idx < digits.length) {
+    const currentLevel = map[digits.charAt(idx)]
+    if(prev.length === 0) {
+      for(let i = 0; i < currentLevel.length; i++) {
+        prev.push(currentLevel[i])
+      }
+    } else {
+      let tmp = []
+      for(let i = 0; i < prev.length; i++) {
+        for(let j = 0; j < currentLevel.length; j++) {
+          tmp.push(prev[i] + currentLevel[j])
+        }
+      }
+      prev = tmp
+    }
+    idx += 1
   }
 
-  const curOptions = map[digits[i]]
-  for(let j = 0; j < curOptions.length; j++) {
-    combind(i + 1, digits, curCombination + curOptions.charAt(j), ret)
-  }
+  return prev
 }
+
+// DFS
+// var letterCombinations = function(digits) {
+//   if(!digits) {
+//     return []
+//   }
+//   const ret = []
+//   combind(0, digits, '', ret)
+//   return ret
+// };
+
+// function combind(i, digits, curCombination, ret) {
+//   if(i == digits.length) {
+//     ret.push(curCombination)
+//     return ret
+//   }
+
+//   const curOptions = map[digits[i]]
+//   for(let j = 0; j < curOptions.length; j++) {
+//     combind(i + 1, digits, curCombination + curOptions.charAt(j), ret)
+//   }
+// }
 
 // console.log(letterCombinations('23'))
 // @lc code=end
