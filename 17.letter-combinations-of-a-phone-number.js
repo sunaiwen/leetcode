bbff12015381d096516d272cf0e9a88798306e3b
sunaiwen <sunivengg@gmail.com>
2020-10-24 23:52:49 +0800

    doc: Time complexity

--- 17.letter-combinations-of-a-phone-number.js
+++ 17.letter-combinations-of-a-phone-number.js
@@ -27,7 +27,12 @@ const map = {
  * 对于 BFS，从第一个输入的层级开始，第一层和第二层混合，然后一二层混合的产物，再和接下来的层混合。
  * 直到最后一层。
  *
- * 对于 DFS，先往最深处一直叠加，然后返回上一层，基于本层再次往深处叠加。依次规律继续进行。
+ *
+ * 时间复杂度：
+ * 最坏情况是重复按了能输出 4 个字母的键，按了 n 次，
+ * 那里面的两个 for 循环就是 4^2，重复 n 次就是 4 * 4 进行 n 次，
+ * 所以是 4 ^ n 平方。
+ *
  */
 var letterCombinations = function(digits) {
   if(!digits) {
@@ -56,7 +61,10 @@ var letterCombinations = function(digits) {
   return prev
 }
 
-// DFS
+/**
+ * DFS
+ * 对于 DFS，先往最深处一直叠加，然后返回上一层，基于本层再次往深处叠加。依次规律继续进行。
+ */
 // var letterCombinations = function(digits) {
 //   if(!digits) {
 //     return []
