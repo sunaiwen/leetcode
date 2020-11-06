3f2900a36ba24c15d2f80aa54db6a32f27ca3c22
sunaiwen <sunivengg@gmail.com>
2020-11-06 23:48:17 +0800

    feat: 784/BFS

--- 784.letter-case-permutation.js
+++ 784.letter-case-permutation.js
@@ -13,39 +13,63 @@
 /**
  * 简单的 dfs，runtime 只打败了 9%... 
  */
-var letterCasePermutation = function(S) {
-  const ret = []
-  dfs(S, 0, [], ret)
-  return ret
-};
+// var letterCasePermutation = function(S) {
+//   const ret = []
+//   dfs(S, 0, [], ret)
+//   return ret
+// };
 
-function dfs(S, startIdx, cur, ret) {
-  if(cur.length === S.length) {
-    ret.push(cur.join(''))
-    return
-  }
+// function dfs(S, startIdx, cur, ret) {
+//   if(cur.length === S.length) {
+//     ret.push(cur.join(''))
+//     return
+//   }
 
-  for(let i = startIdx; i < S.length; i++) {
-    const char = S.charAt(i)
+//   for(let i = startIdx; i < S.length; i++) {
+//     const char = S.charAt(i)
+//     const charCode = char.charCodeAt(0)
+//     cur.push(char)
+//     dfs(S, i + 1, cur, ret)
+//     cur.pop()
+
+//     if(65 <= charCode && charCode <= 90) {
+//       cur.push(char.toLowerCase())
+//       dfs(S, i + 1, cur, ret)
+//       cur.pop()
+//     }
+
+//     if(97 <= charCode && charCode <= 122 ) {
+//       cur.push(char.toUpperCase())
+//       dfs(S, i + 1, cur, ret)
+//       cur.pop()
+//     }
+//   }
+// }
+
+var letterCasePermutation = function (S) {
+  const queue = ['']
+  const charList = S.split('')
+
+  while(charList.length) {
+    const curLevelLenth = queue.length
+    const char = charList.shift()
     const charCode = char.charCodeAt(0)
-    cur.push(char)
-    dfs(S, i + 1, cur, ret)
-    cur.pop()
 
-    if(65 <= charCode && charCode <= 90) {
-      cur.push(char.toLowerCase())
-      dfs(S, i + 1, cur, ret)
-      cur.pop()
-    }
+    for(let i = 0; i < curLevelLenth; i++) {
+      const curLevelItem = queue.shift()
+      queue.push(curLevelItem + char)
 
-    if(97 <= charCode && charCode <= 122 ) {
-      cur.push(char.toUpperCase())
-      dfs(S, i + 1, cur, ret)
-      cur.pop()
+      if(65 <= charCode && charCode <= 90) {
+        queue.push(curLevelItem + char.toLowerCase())
+      } else if(97 <= charCode && charCode <= 122 ) {
+        queue.push(curLevelItem + char.toUpperCase())
+      }
     }
   }
+
+  return queue
 }
 
-// letterCasePermutation('C')
+// letterCasePermutation('a1b2')
 // @lc code=end
 
